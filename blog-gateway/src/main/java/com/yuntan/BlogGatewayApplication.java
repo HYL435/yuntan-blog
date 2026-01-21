ce9b4be4fce4f1878ae0945565940144766320ed
苜玥 <2929028544@qq.com>
2026-01-21 22:59:30 +0800

    个人博客系统框架搭建完成

---
+++ blog-gateway/src/main/java/com/yuntan/BlogGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yuntan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogGatewayApplication.class, args);
+    }
+
+}
